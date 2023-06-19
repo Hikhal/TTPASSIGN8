0164ed67939167f1df9a891b1ca4ed8ee90ecbea
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-18 22:25:55 -0400

    comments added to App.js & searchGif.jsx

--- gifapi/src/App.js
+++ gifapi/src/App.js
@@ -3,8 +3,10 @@ import SearchGif from './components/searchGif';
 import React, { useState } from 'react';
 
 function App() {
+    // Using React's useState hook to create a state variable and setter for the search term
     const [term, setTerm] = useState('');
-
+    
+    // This function updates the state variable 'term' whenever the input field changes
     const updateTerm = (event) => {
         setTerm(event.target.value);
     }
--- gifapi/src/components/searchGif.jsx
+++ gifapi/src/components/searchGif.jsx
@@ -8,9 +8,11 @@ const SearchGif = (props) => {
     useEffect(() => {
         const fetchGifs = async () => {
             let api;
+            // If the user has entered a search term, use the search API
             if (props.txt) {
                 api = 'https://api.giphy.com/v1/gifs/search?api_key=ZRN9GheLBgOvFK5pYUcZlb8Mh6mkC9EA&q=' + props.txt + '&limit=2&offset=0&rating=g&lang=en&bundle=messaging_non_clips';
             } else {
+            // If no search term was entered, fetch the trending gifs
                 api = 'https://api.giphy.com/v1/gifs/trending?api_key=ZRN9GheLBgOvFK5pYUcZlb8Mh6mkC9EA&limit=6&offset=0&rating=g&bundle=messaging_non_clips%22';
             }
 
@@ -22,15 +24,17 @@ const SearchGif = (props) => {
             }
         }
         fetchGifs();
+        // This useEffect is dependent on props.txt. When props.txt changes, the useEffect will run again
     }, [props.txt]);
 
     return (
         <div>
             {searchRes.map((item) => {
+                 // For each gif in searchRes, render a GifCards component with the gif as a prop.
                 return <GifCards key={item.id} gif={item} />
             })}
         </div>
     );
 }
-
+ 
 export default SearchGif;
